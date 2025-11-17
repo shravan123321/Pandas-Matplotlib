50198f709426038902a99a15949a969c8b384876
Shravan7033 <kumarshravan40478@gmail.com>
2025-11-17 08:48:20 +0530

    Array Visulition the data

---
+++ Array/q1.py
@@ -0,0 +1,5 @@
+# define the pandas implementations and then so 1D array data define 
+import pandas as pd
+d={"key":[1,2,3,4,5,6,7,8]}
+d1=pd.DataFrame(d)
+print("data view the 1D Array:\n",d1)
---
+++ Array/q2.py
@@ -0,0 +1,8 @@
+# define the pandas and then so implements the 2D array implemets 
+import pandas as pd
+d1={
+    "key":[1,2,3,4],
+    "name":["Shravan","karan","sudhir","shamir"]
+}
+two=pd.DataFrame(d1)
+print("two dimensional Array:\n",two)
